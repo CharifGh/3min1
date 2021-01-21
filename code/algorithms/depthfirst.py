a7d0761d31a7476a36b3296613e2edbe35ead568
CharifGh <charif@hotmail.com>
2021-01-21 16:54:20 +0100

    started depthfirst

--- code/algorithms/depthfirst.py
+++ code/algorithms/depthfirst.py
@@ -8,13 +8,23 @@ def __init__(self, district, connections)
     self.district = copy.deepcopy(district)
     self.connections = connections
     
-
-
 def get_next_connection(self):
+    # kijken naar verschillende connecties? 
     connections = self.connections
-
 
 def valid_connection(self):
     self.traversal = []
+
+def build_connections(self, district, battery, house):
+    # zelf bouwen hier met een for loop tijdens de search?
+    pass
+
 # check connections that are visited and not visited
 
+# start at 0 and arbitrary go to node 
+# dont revisit visited nodes
+# dont visit nodes already visiting
+# backtrack  and continue as long edges can be picked
+# start 
+
+# start at node check if neighbour is not visited if not put in stack mark current node as visited go to neighbour if no neighbour put node as visited
