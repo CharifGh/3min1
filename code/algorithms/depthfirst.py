60a5a206334f19cd2c9d43a57391bcc19498fcf0
CharifGh <charif@hotmail.com>
2021-01-21 16:38:21 +0100

    started depthfirst

---
+++ code/algorithms/depthfirst.py
@@ -0,0 +1,20 @@
+import copy
+
+
+class DepthFirst:
+"""
+"""
+def __init__(self, district, connections)
+    self.district = copy.deepcopy(district)
+    self.connections = connections
+    
+
+
+def get_next_connection(self):
+    connections = self.connections
+
+
+def valid_connection(self):
+    self.traversal = []
+# check connections that are visited and not visited
+
