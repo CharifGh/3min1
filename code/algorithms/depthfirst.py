c124b9545c8d6331caa91d1ea1186e3d6165f9d7
CharifGh <charif@hotmail.com>
2021-01-24 15:21:16 +0100

    some minor changes to DFS

--- code/algorithms/depthfirst.py
+++ code/algorithms/depthfirst.py
@@ -3,14 +3,14 @@ import copy
 
 class DepthFirst:
     """Check for connections if visited append them to list."""
-    def __init__(self, connections):
-        self.connections = copy.deepcopy(connection)
+    def __init__(self, connection):
+        self.connection = copy.deepcopy(connection)
         self.visited_connections = []
  
     def get_next_connection(self, connection):
         """check if connection in visited_connections else append connection to visited_connections"""
-        if connection not in visited_connections:
-            visited_connections.append(connection)
-            for next_con in visited_connections[connection]:
-                get_next_connection(connection)
+        if self.connection not in visited_connections:
+            visited_connections.append(self.connection)
+            for next_con in visited_connections[self.connection]:
+                get_next_connection(self.connection)
         return visited_connections
