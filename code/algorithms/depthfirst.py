3c08cae3dfc57aa8232775d35a1351904670b05b
CharifGh <charif@hotmail.com>
2021-01-24 15:18:51 +0100

    some minor changes to DFS

--- code/algorithms/depthfirst.py
+++ code/algorithms/depthfirst.py
@@ -2,29 +2,15 @@ import copy
 
 
 class DepthFirst:
-"""
-"""
-def __init__(self, district, connections)
-    self.district = copy.deepcopy(district)
-    self.connections = connections
-    
-def get_next_connection(self):
-    # kijken naar verschillende connecties? 
-    connections = self.connections
-
-def valid_connection(self):
-    self.traversal = []
-
-def build_connections(self, district, battery, house):
-    # zelf bouwen hier met een for loop tijdens de search?
-    pass
-
-# check connections that are visited and not visited
-
-# start at 0 and arbitrary go to node 
-# dont revisit visited nodes
-# dont visit nodes already visiting
-# backtrack  and continue as long edges can be picked
-# start 
-
-# start at node check if neighbour is not visited if not put in stack mark current node as visited go to neighbour if no neighbour put node as visited
+    """Check for connections if visited append them to list."""
+    def __init__(self, connections):
+        self.connections = copy.deepcopy(connection)
+        self.visited_connections = []
+ 
+    def get_next_connection(self, connection):
+        """check if connection in visited_connections else append connection to visited_connections"""
+        if connection not in visited_connections:
+            visited_connections.append(connection)
+            for next_con in visited_connections[connection]:
+                get_next_connection(district,connection)
+        return visited_connections
