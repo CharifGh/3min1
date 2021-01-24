239e1b8f8d227633480494e702f8b115733acd80
CharifGh <charif@hotmail.com>
2021-01-24 15:27:57 +0100

    some minor changes to DFS

--- code/algorithms/depthfirst.py
+++ code/algorithms/depthfirst.py
@@ -14,3 +14,16 @@ class DepthFirst:
             for next_con in visited_connections[self.connection]:
                 get_next_connection(self.connection)
         return visited_connections
+
+    def valid_solution(self):
+        """Do some calculation to check if connection is valid"""
+        pass
+    
+    def run(self):
+        """Run algorith untill all connections are made"""
+        while self.connection:
+            new_connection = self.get_next_connection()
+
+            # check if calculation has valid solution break the algorithm else continue making connections
+            if valid_solution():
+                break
