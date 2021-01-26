098ef4652aedf4e0fd72319c5abf2515405750be
CharifGh <charif@hotmail.com>
2021-01-26 14:10:47 +0100

    simmulated annealing

--- code/algorithms/simulatedannealing.py
+++ code/algorithms/simulatedannealing.py
@@ -1,37 +1,21 @@
 import copy
 
 
-class DepthFirst:
+class SimmulatedAnnealing:
     """Check for connections if visited append them to list."""
-    def __init__(self, district, connection):
+    def __init__(self, district):
         self.district = copy.deepcopy(district)
-        self.visited_connections = []
-        self.connection = copy.deepcopy(connection)
-        self.solutions=None
-        self.value= float('inf')
+        self.result = []
+        self.start = 1000
+        self.change = 0.97
 
-        
-    def get_next_connection(self):
-        """check if connection in visited_connections else append connection to visited_connections"""
-        connection = self.connection
-        if connection not in self.visited_connections:
-            self.visited_connections.append(connection)
-            for next_con in self.visited_connections[connection]:
-               dfs= get_next_connection(self.district, self.connection)
-
-        return self.visited_connections
+    def connections(self):
+        while self.start < 0.0:
+            self.district.make_cables()
 
 
-    def run(self):
-        """Run algorith untill all connections are made"""
-        while self.district:
-            new_connection = self.get_next_connection()
 
-     
-            if self.district.check_validity == True:
-                break
-
-
+  
             #bepaalde state
             # state-space
 
